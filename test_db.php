a0b9bc7e1b0938dd458ed9498d15757ceddbffff
Jarne <jarne.kampshof@student.graafschapcollege.nl>
2026-03-13 09:20:59 +0100

    test

---
+++ test_db.php
@@ -0,0 +1,10 @@
+<?php
+require 'etc/config.php';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Database connection successful!";
+?>
