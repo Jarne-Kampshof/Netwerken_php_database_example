228d60affa1165e434c277c2916ace13ef6333bd
Jarne <jarne.kampshof@student.graafschapcollege.nl>
2026-03-13 09:42:06 +0100

    test

--- etc/config_example.php
+++ etc/config_example.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL server.
 Please change this file to config.php and change these defines: */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'username');
-define('DB_PASSWORD', 'QBLGSnRNdpZuKdbuerX6');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'databasename');
  
 /* Attempt to connect to MySQL database */
